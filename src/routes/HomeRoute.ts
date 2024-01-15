6ec7b03b7fabb111778d717a4c735ba3561ac0d1
iris <60985521+KaiAF@users.noreply.github.com>
2024-01-14 19:24:27 -0600

    find users by vanity url

--- src/routes/HomeRoute.ts
+++ src/routes/HomeRoute.ts
@@ -1,10 +1,43 @@
 import express from 'express';
 import renderPage from '../utils/RenderPage';
+import { User } from '../managers/database/types/UserTypes';
+import { querySync } from '../managers/database/MySQLConnection';
 
 const app = express.Router();
 
+async function findVanity(vanity: string): Promise<User | null> {
+  const linkedProfile = (await querySync('select * from linkedAccounts where vanityUrl = ? and linked = 1', [vanity.toLowerCase()]))[0];
+  if (!linkedProfile) return null;
+  const user: User = (await querySync('select * from profiles where uuid = ? and banned = 0', [linkedProfile.uuid]))[0];
+
+  return user;
+}
+
 app.get('/', (req, res) => {
   renderPage(req, res, 'home', {});
+});
+
+// legacy vanity urls
+app.get('/v/:vanity', async function (req, res) {
+  try {
+    const user = await findVanity(req.params.vanity);
+    if (!user) return res.sendStatus(404);
+    res.send(user);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
+});
+
+app.get('/:vanity', async function (req, res) {
+  try {
+    const user = await findVanity(req.params.vanity);
+    if (!user) return res.sendStatus(404);
+    res.send(user);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 });
 
 app.get('/contact', (req, res) => {
