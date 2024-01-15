4bbc4280ae36bd45230d194a027ef8a3d4177283
iris <60985521+KaiAF@users.noreply.github.com>
2024-01-14 19:15:17 -0600

    add /contact

--- src/routes/HomeRoute.ts
+++ src/routes/HomeRoute.ts
@@ -7,4 +7,11 @@ app.get('/', (req, res) => {
   renderPage(req, res, 'home', {});
 });
 
+app.get('/contact', (req, res) => {
+  res.write('You can contact me at these places:\n');
+  res.write('Discord: https://discord.gg/ytrKev7xZD\n');
+  res.write('Email: support@livzmc.net\n');
+  res.send();
+});
+
 export default app;
