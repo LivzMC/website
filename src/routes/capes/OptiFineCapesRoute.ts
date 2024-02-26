b8d6fbd85588e86f77017b7b1eadd3f07fcb12b7
iris <60985521+KaiAF@users.noreply.github.com>
2024-02-26 06:58:03 -0600

    Add similar banner designs

--- src/routes/capes/OptiFineCapesRoute.ts
+++ src/routes/capes/OptiFineCapesRoute.ts
@@ -8,9 +8,12 @@ import ErrorManager from '../../managers/ErrorManager';
 import { querySync } from '../../managers/database/MySQLConnection';
 import { isFileCacheExpired } from '../../utils/Utils';
 import { Banner } from '../../managers/database/types/OptiFineCapeTypes';
+import { Account } from '../../managers/database/types/AccountTypes';
 
 const app = express.Router();
-const BannerUserLengthCache = new NodeCache();
+
+const BANNER_USER_LENGTH_CACHE = new NodeCache();
+const SIMILAR_BANNERS_CACHE = new NodeCache();
 
 type SearchOptions = {
   page: number,
@@ -42,6 +45,23 @@ async function generateUsersCache(bannerId: string, path: string): Promise<void>
     [bannerId]);
 
   await fsp.writeFile(path, JSON.stringify(profiles));
+}
+
+async function findSimilarBanners(cleanUrl: string, isBanner: boolean = true): Promise<Banner[]> {
+  if (cleanUrl.toLowerCase().startsWith('optifine ') || cleanUrl.toLowerCase().startsWith('custom ')) return [];
+  const CACHE_KEY = cleanUrl + '-' + isBanner;
+  if (SIMILAR_BANNERS_CACHE.has(CACHE_KEY)) return SIMILAR_BANNERS_CACHE.get(CACHE_KEY) as unknown as Banner[];
+
+  const letters: string[] = [...cleanUrl.match(/.{1,2}/g) || ''];
+  let layers = letters.length > 4 ? '%' : '';
+
+  for (let i = letters.length > 4 ? 1 : 0; i < letters.length; i++) {
+    layers = layers.concat(`_${letters[i][1]}`);
+  }
+
+  const banners: Banner[] = await querySync('select bannerId, cleanUrl from banners where isBanner = ? and removed = 0 and cleanUrl like binary ? limit 100', [isBanner ? '1' : '0', layers]);
+  SIMILAR_BANNERS_CACHE.set(CACHE_KEY, banners, 60 * 15);
+  return banners;
 }
 
 app.get('/', async function (req, res) {
@@ -76,31 +96,48 @@ app.get('/', async function (req, res) {
 
 app.get('/:bannerId', async function (req, res) {
   try {
+    const account: Account = res.locals.account;
     const banner: Banner = (await querySync('select * from banners where bannerId = ?', [req.params.bannerId]))[0];
     if (!banner || banner.removed) return res.status(404).send('Banner not found');
     const path = `cache/capes/banner/${banner.bannerId}-users.json`;
     const isCached = fs.existsSync(path); // cache for 5 minutes
 
     if (!isCached) await generateUsersCache(banner.bannerId, path);
-    const length = BannerUserLengthCache.has(banner.bannerId) ? BannerUserLengthCache.get(banner.bannerId) : (await querySync('SELECT count(capeId) from profileOFCapes WHERE capeId = ? and hidden = 0', [banner.bannerId]))[0]['count(capeId)'];
+    const length = BANNER_USER_LENGTH_CACHE.has(banner.bannerId) ? BANNER_USER_LENGTH_CACHE.get(banner.bannerId) : (await querySync('SELECT count(capeId) from profileOFCapes WHERE capeId = ? and hidden = 0', [banner.bannerId]))[0]['count(capeId)'];
 
     const data = await fsp.readFile(path);
     const profiles = JSON.parse(data.toString());
     const more = profiles.length > 500;
     if (more) profiles.length = 500;
 
+    let similarBanners: Banner[] = [];
+    if (banner.cleanUrl) {
+      if (account && account.permission > 7) {
+        similarBanners = similarBanners.concat(await findSimilarBanners(banner.cleanUrl, req.query.admin && req.query.admin.toString() === 'true' ? false : true));
+      } else {
+        if (banner.isBanner) {
+          similarBanners = similarBanners.concat(await findSimilarBanners(banner.cleanUrl, true));
+        }
+      }
+    }
+
+    if (similarBanners.length > 0) {
+      similarBanners = similarBanners.filter(c => c.bannerId !== banner.bannerId);
+    }
+
     renderPage(req, res, './capes/view/optifine', {
       banner,
       profiles,
       more,
       length,
+      similarBanners,
     });
 
     if (isCached && isFileCacheExpired(path, 60 * 5)) {
       await generateUsersCache(banner.bannerId, path);
     }
 
-    if (!BannerUserLengthCache.has(banner.bannerId)) BannerUserLengthCache.set(banner.bannerId, length, 60 * 5);
+    if (!BANNER_USER_LENGTH_CACHE.has(banner.bannerId)) BANNER_USER_LENGTH_CACHE.set(banner.bannerId, length, 60 * 5);
   } catch (e) {
     console.error(e);
     new ErrorManager(req, res, e as Error).write();
